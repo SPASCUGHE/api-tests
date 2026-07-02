5e6aeb0b78bfa310784bf50aabcf6946317d657d
sebastian.pascu <sebastianpascu@Sebastians-MacBook-Pro.local>
2026-07-03 00:46:48 +0300

    add api tests

--- src/env/manager.ts
+++ src/env/manager.ts
@@ -3,4 +3,6 @@ import { join } from 'path';
 
 config({ path: join(process.cwd(), 'src', 'env', `${process.env.ENV || 'dev'}.env`) });
 
-export const API_BASEURL = process.env.API_BASEURL as string;
+const DEFAULT_API_BASEURL = 'https://jsonplaceholder.typicode.com';
+
+export const API_BASEURL = process.env.API_BASEURL || DEFAULT_API_BASEURL;
